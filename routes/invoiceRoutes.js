08fa33422b688cdf1d17fdd1bb58061d53503836
Shivam Shukla <shivamshukla8103@gmail.com>
2025-12-02 12:54:06 +0530

    Initial invoice app

--- routes/invoiceRoutes.js
+++ routes/invoiceRoutes.js
@@ -38,9 +38,14 @@ router.post('/generate', async (req, res) => {
 
     // launch puppeteer
     const browser = await puppeteer.launch({
-      headless: 'new',
-      args: ['--no-sandbox', '--disable-setuid-sandbox'],
-    });
+  headless: 'new',
+  args: [
+    '--no-sandbox',
+    '--disable-setuid-sandbox',
+    '--disable-dev-shm-usage',
+  ],
+});
+
 
     const page = await browser.newPage();
     await page.setContent(html, { waitUntil: 'networkidle0' });
